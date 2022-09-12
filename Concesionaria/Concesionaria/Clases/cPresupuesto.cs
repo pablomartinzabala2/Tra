78ec710de890ffd0a71b95e0be45bf46404917df
Pablo Zabala <PABLOMARTINZABALA2@GMAIL.COM>
2022-09-12 19:33:45 -0300

    ss
    ss

---
+++ Concesionaria/Concesionaria/Clases/cPresupuesto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ApplicationBlocks.Data;
+using System.Data.SqlClient;
+namespace Concesionaria.Clases
+{
+    public class cPresupuesto
+    {
+        public void Insertar(Int32? CodAuto,Int32? CodCliente,DateTime Fecha,Double Total, string sTotal)
+        {
+            string sql = "Insert into Presupuesto(";
+            sql = sql + "CodAuto,CodCliente,Fecha,Total,sTotal";
+            sql = sql + ")";
+            sql = sql + " Values(";
+            sql = sql + CodAuto.ToString();
+            sql = sql + "," + CodCliente.ToString();
+            sql = sql + "," + "'" + Fecha.ToShortDateString() + "'";
+            sql = sql + "," + Total.ToString().Replace(",", ".");
+            sql = sql + "," + "'" + sTotal + "'";
+            sql = sql + ")";
+            cDb.ExecutarNonQuery(sql);
+        }
+    }
+}
