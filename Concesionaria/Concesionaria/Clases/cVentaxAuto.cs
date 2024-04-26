c75baa2f4c126a82a391bd1880ae37af7bdce751
Pablo <44281098+pablomartinzabala2@users.noreply.github.com>
2024-04-27 00:40:23 +0200

    ss
    ss

--- Concesionaria/Concesionaria/Clases/cVentaxAuto.cs
+++ Concesionaria/Concesionaria/Clases/cVentaxAuto.cs
@@ -14,11 +14,13 @@ namespace Concesionaria.Clases
         public DataTable GetAutosxCodVenta(Int32 CodVenta)
         {
             string sql = "select va.CodAuto,a.Patente,a.Descripcion";
-            sql = sql + ",m.Nombre,va.Importe,sa.CodStock";
-            sql = sql + " from VentasxAuto va, Auto a, StockAuto sa";
+            // sql = sql + ",m.Nombre,va.Importe,sa.CodStock";
+            sql = sql + ",m.Nombre,va.Importe ";
+            // sql = sql + " from VentasxAuto va, Auto a, StockAuto sa";
+            sql = sql + " from VentasxAuto va, Auto a ";
             sql = sql + " ,Marca m";
             sql = sql + " where va.CodAuto = a.CodAuto ";
-            sql = sql + " and va.CodAuto =sa.CodAuto    ";
+          //  sql = sql + " and va.CodAuto =sa.CodAuto    ";
             sql = sql + " and m.CodMarca = a.CodMarca ";
             sql = sql + " and va.CodVenta=" + CodVenta.ToString();
             return cDb.ExecuteDataTable(sql);
